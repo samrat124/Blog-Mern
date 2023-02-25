a7fc5093a650b2c99196a0f67991a310c20fdcf5
samrat kumar <sam9999950@gmail.com>
2023-02-25 23:13:15 +0530

    server completed

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import * as dotenv from 'dotenv';
+dotenv.config({path :"./.env"});
+import postRoutes from './routes/posts.js'
+import userRoutes from './routes/user.js'
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
+
+app.get('/', (_, res) => res.send('Hello to Blogpost API'))
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+	.connect(process.env.CONNECTION_URL)
+	.then(console.log('Connected to MongoDB Database 🌐'))
+	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT} 🚀`)))
+	.catch((error) => console.log(`❎ Server did not connect ⚠️\n${error}`))
